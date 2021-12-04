745bf76274230cdacb8d118e4badb30e2142f2fe
java110 <928255095@qq.com>
2021-12-04 16:28:10 +0800

    优化代码

--- web/components/admin/deleteBusinessDockerfile/deleteBusinessDockerfile.js
+++ web/components/admin/deleteBusinessDockerfile/deleteBusinessDockerfile.js
@@ -1,52 +1,49 @@
-(function(vc,vm){
+(function(vc, vm) {
 
     vc.extends({
-        data:{
-            deleteBusinessDockerfileInfo:{
+        data: {
+            deleteBusinessDockerfileInfo: {
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function() {
 
-         },
-         _initEvent:function(){
-             vc.on('deleteBusinessDockerfile','openDeleteBusinessDockerfileModal',function(_params){
+        },
+        _initEvent: function() {
+            vc.on('deleteBusinessDockerfile', 'openDeleteBusinessDockerfileModal', function(_params) {
 
                 vc.component.deleteBusinessDockerfileInfo = _params;
                 $('#deleteBusinessDockerfileModel').modal('show');
-
             });
         },
-        methods:{
-            deleteBusinessDockerfile:function(){
-                vc.component.deleteBusinessDockerfileInfo.communityId=vc.getCurrentCommunity().communityId;
+        methods: {
+            deleteBusinessDockerfile: function() {
                 vc.http.apiPost(
                     'businessDockerfile.deleteBusinessDockerfile',
-                    JSON.stringify(vc.component.deleteBusinessDockerfileInfo),
-                    {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                    JSON.stringify(vc.component.deleteBusinessDockerfileInfo), {
+                        emulateJSON: true
+                    },
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
                             //关闭model
                             $('#deleteBusinessDockerfileModel').modal('hide');
-                            vc.emit('businessDockerfileManage','listBusinessDockerfile',{});
-                            return ;
+                            vc.emit('businessDockerfileManage', 'listBusinessDockerfile', {});
+                            return;
                         }
                         vc.message(_json.msg);
-                     },
-                     function(errInfo,error){
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                         vc.message(json);
 
-                     });
+                    });
             },
-            closeDeleteBusinessDockerfileModel:function(){
+            closeDeleteBusinessDockerfileModel: function() {
                 $('#deleteBusinessDockerfileModel').modal('hide');
             }
         }
     });
 
-})(window.vc,window.vc.component);
+})(window.vc, window.vc.component);
