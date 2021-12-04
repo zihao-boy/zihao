4fadcc6da9b86cefcf1faa6b80467d4c76306702
java110 <928255095@qq.com>
2021-12-04 14:20:05 +0800

    优化代码

--- web/components/admin/deleteBusinessPackage/deleteBusinessPackage.js
+++ web/components/admin/deleteBusinessPackage/deleteBusinessPackage.js
@@ -1,52 +1,50 @@
-(function(vc,vm){
+(function(vc, vm) {
 
     vc.extends({
-        data:{
-            deleteBusinessPackageInfo:{
+        data: {
+            deleteBusinessPackageInfo: {
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function() {
 
-         },
-         _initEvent:function(){
-             vc.on('deleteBusinessPackage','openDeleteBusinessPackageModal',function(_params){
+        },
+        _initEvent: function() {
+            vc.on('deleteBusinessPackage', 'openDeleteBusinessPackageModal', function(_params) {
 
                 vc.component.deleteBusinessPackageInfo = _params;
                 $('#deleteBusinessPackageModel').modal('show');
 
             });
         },
-        methods:{
-            deleteBusinessPackage:function(){
-                vc.component.deleteBusinessPackageInfo.communityId=vc.getCurrentCommunity().communityId;
+        methods: {
+            deleteBusinessPackage: function() {
                 vc.http.apiPost(
                     '/soft/deleteBusinessPackages',
-                    JSON.stringify(vc.component.deleteBusinessPackageInfo),
-                    {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                    JSON.stringify(vc.component.deleteBusinessPackageInfo), {
+                        emulateJSON: true
+                    },
+                    function(json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
                         let _json = JSON.parse(json);
                         if (_json.code == 0) {
                             //关闭model
                             $('#deleteBusinessPackageModel').modal('hide');
-                            vc.emit('businessPackageManage','listBusinessPackage',{});
-                            return ;
+                            vc.emit('businessPackageManage', 'listBusinessPackage', {});
+                            return;
                         }
                         vc.message(_json.msg);
-                     },
-                     function(errInfo,error){
+                    },
+                    function(errInfo, error) {
                         console.log('请求失败处理');
                         vc.message(json);
 
-                     });
+                    });
             },
-            closeDeleteBusinessPackageModel:function(){
+            closeDeleteBusinessPackageModel: function() {
                 $('#deleteBusinessPackageModel').modal('hide');
             }
         }
     });
 
-})(window.vc,window.vc.component);
+})(window.vc, window.vc.component);
