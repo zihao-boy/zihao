0e5347afcfaccfb3e212249168383e4b75745bba
java110 <928255095@qq.com>
2021-12-08 16:19:05 +0800

    优化代码

--- common/containerScheduling/defalutContainerScheduling/DefalutContainerScheduling.go
+++ common/containerScheduling/defalutContainerScheduling/DefalutContainerScheduling.go
@@ -103,7 +103,7 @@ func doStartContainer(host *host.HostDto, appServiceDto *appService.AppServiceDt
 		ip = ip[0:strings.Index(ip, ":")]
 	}
 
-	ip += (":" + string(config.Slave))
+	ip += (":" + strconv.FormatInt(int64(config.Slave),10))
 
 	resp, err := httpReq.Post("http://"+ip+"/app/slave/startContainer", data, nil)
 	if err != nil {
