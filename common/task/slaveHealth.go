bc9a13a15f8df8cb3b732f7d65084b5c95e2a9fc
java110 <928255095@qq.com>
2021-12-08 14:58:23 +0800

    todo heartbeat int to string

--- common/task/slaveHealth.go
+++ common/task/slaveHealth.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shopspring/decimal"
+	"strconv"
 	"time"
 
 	"github.com/zihao-boy/zihao/common/httpReq"
@@ -26,27 +27,40 @@ func doSlaveHealth() {
 
 	//获取cpu
 	cpuCore, _ := cpu.Counts(true)
-	cpuPercent, _:= cpu.Percent(time.Second, false)
+	cpuPercent, _ := cpu.Percent(time.Second, false)
 
 	cpuPercentDec := decimal.NewFromFloat(cpuPercent[0])
 	cpuPercentDec = cpuPercentDec.Mul(decimal.NewFromInt(int64(cpuCore)))
 
-	useCpu,_ :=cpuPercentDec.Float64()
+	useCpu, _ := cpuPercentDec.Float64()
 	// 获取内存
 	totalMem, _ := mem.VirtualMemory()
+	totalMemDec := decimal.NewFromInt(int64(totalMem.Total))
+	totalMemDec = totalMemDec.Div(decimal.NewFromInt(1024 * 1024))
+	totalMemValue, _ := totalMemDec.Float64()
 
+	totalMemUseDec := decimal.NewFromInt(int64(totalMem.Used))
+	totalMemUseDec = totalMemUseDec.Div(decimal.NewFromInt(1024 * 1024))
+	totalMemUseValue, _ := totalMemUseDec.Float64()
 	// 获取磁盘
 	totalDisk, _ := disk.Usage("/")
 
+	totalDiskDec := decimal.NewFromInt(int64(totalDisk.Total))
+	totalDiskDec = totalDiskDec.Div(decimal.NewFromInt(1024 * 1024))
+	totalDiskValue, _ := totalDiskDec.Float64()
+
+	totalDiskUseDec := decimal.NewFromInt(int64(totalDisk.Used))
+	totalDiskUseDec = totalDiskUseDec.Div(decimal.NewFromInt(1024 * 1024))
+	totalDiskUseValue, _ := totalMemUseDec.Float64()
 
 	data := map[string]interface{}{
 		"hostId":  slaveId,
-		"cpu":     cpuCore,
-		"mem":     totalMem.Total,
-		"disk":    totalDisk.Total,
-		"useCpu":  useCpu,
-		"useMem":  totalMem.Used,
-		"useDisk": totalDisk.Used,
+		"cpu":     strconv.FormatInt(int64(cpuCore), 10),
+		"mem":     fmt.Sprintf("%.2f",totalMemValue),
+		"disk":    fmt.Sprintf("%.2f",totalDiskValue),
+		"useCpu":  fmt.Sprintf("%.2f",useCpu),
+		"useMem":  fmt.Sprintf("%.2f",totalMemUseValue),
+		"useDisk": fmt.Sprintf("%.2f",totalDiskUseValue),
 	}
 	resp, err := httpReq.Post(url, data, nil)
 	if err != nil {
