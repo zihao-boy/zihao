9559f88d2dc849cacda2accb85c5dd38b28394f0
java110 <928255095@qq.com>
2021-12-12 17:45:19 +0800

    test

--- web/pages/admin/fileManager/fileManager.js
+++ web/pages/admin/fileManager/fileManager.js
@@ -20,7 +20,36 @@
 
         },
         methods: {
+            _changeDir: function(_file) {
+                if (_file.groupName != 'd') {
+                    return;
+                }
+                if ($that.fileManagerInfo.curPath.endsWith('/')) {
+                    $that.fileManagerInfo.curPath += _file.name;
+                } else {
+                    $that.fileManagerInfo += ("/" + _file.name);
+                }
+                $that._listFiles();
+            },
+            _parentDir: function() {
+                //如果是跟目录 直接显示根目录
+                if (!$that.fileManagerInfo.curPath || $that.fileManagerInfo.curPath == '/') {
+                    $that.fileManagerInfo.curPath = "/";
+                    $that._listFiles();
+                    return;
+                }
 
+                if ($that.fileManagerInfo.curPath.endsWith('/')) {
+                    $that.fileManagerInfo.curPath = $that.fileManagerInfo.curPath.substring(0, $that.fileManagerInfo.curPath.length - 1);
+                }
+                $that.fileManagerInfo.curPath = $that.fileManagerInfo.curPath.substring(0, $that.fileManagerInfo.curPath.lastIndexOf('/'));
+
+                //如果返回到最后还是根目录 那还是 显示根目录
+                if (!$that.fileManagerInfo.curPath) {
+                    $that.fileManagerInfo.curPath = "/";
+                }
+                $that._listFiles();
+            },
 
             _listFiles: function() {
                 if (!$that.fileManagerInfo.curPath) {
