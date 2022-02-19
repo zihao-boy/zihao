1d26772cd57f27557695bbddb9af751f492a75a5
java110 <928255095@qq.com>
2022-02-19 14:34:55 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sh compile.sh
+
+rm -rf /zihao/master/web
+
+rm -rf /zihao/master/zihao
+
+rm -rf /zihao/slave/slave*
+
+cp -r ../zihao_release/web /zihao/master/
+
+cp -r ../zihao_release/zihao /zihao/master/
+
+cp -r ../zihao_release/web/download/slave.tar /zihao/slave/
+
+chmod u+x /zihao/master/zihao
+
+tar -xvf /zihao/slave/slave.tar -C /zihao/slave/
+
+chmod u+x /zihao/slave/slave
+
+sh /zihao/slave/startSlave.sh &
+
+sh /zihao/master/restart_zihao.sh &
