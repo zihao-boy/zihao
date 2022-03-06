15a5a4a1ae47eb2aad360f4124d020f54005b471
wuxw <928255095@qq.com>
2022-03-06 22:09:23 +0800

    optimize shell script

--- restart_zihao.sh
+++ restart_zihao.sh
@@ -4,11 +4,9 @@
 
 # kill zihao process
 ps -ef | grep ./zihao | grep -v 'grep' | grep -v 'restart_zihao.sh' | awk '{print $2}' | xargs kill -9
-
-chmod u+x zihao
-
-# start zihao
 cd /zihao/master
+chmod u+x zihao
+# start zihao
 
 ./zihao > zihao.log &
 
