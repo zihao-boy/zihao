d765b41c1048c669651f8b786c995ed07b98f2ab
wuxw <928255095@qq.com>
2022-03-06 21:26:53 +0800

    optimize slave health

--- common/task/slaveHealth.go
+++ common/task/slaveHealth.go
@@ -63,7 +63,7 @@ func doSlaveHealth() {
 		"mem":    fmt.Sprintf("%.2f", totalMemValue),
 		"disk":   fmt.Sprintf("%.2f", totalDiskValue),
 		//"useCpu":     fmt.Sprintf("%.2f", useCpu),
-		"useCpu":     cpuPercent,
+		"useCpu":     fmt.Sprintf("%.2f",cpuPercent),
 		"useMem":     fmt.Sprintf("%.2f", totalMemUseValue),
 		"useDisk":    fmt.Sprintf("%.2f", totalDiskUseValue),
 		"containers": relContainers,
