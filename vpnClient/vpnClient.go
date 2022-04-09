fb22fa88ba93b1adc8893ea7a6bed9067312525e
wuxw <928255095@qq.com>
2022-04-09 14:41:44 +0800

    clinet

--- vpnClient/vpnClient.go
+++ vpnClient/vpnClient.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/zihao-boy/zihao/common/vpn/client"
+	"github.com/zihao-boy/zihao/common/innerNet/client"
 	"github.com/zihao-boy/zihao/config"
 	"github.com/zihao-boy/zihao/entity/dto/vpn"
 	"sync"
